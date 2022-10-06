3ad080049f149df22423d2d7782e676f37f72fb8
StAgony <santiago_almada@outlook.com>
2022-10-06 06:01:56 -0300

    html y css casi terminado

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log ('hola que hace')
+
+/* <div class="nombre"><p>putomon</p></div>
+                <div class="up">
+                    <div class="foto-contenedor"><img class="foto" src="./img/35.png" alt="yea bitch"></div>
+                        <div class="tipos">
+                            <p>Tipos:</p>
+                            <ul>
+                                <li> verdura</li>
+                                <li> roca</li>
+                                <li> heavy metal</li>
+                                <li> caca</li>
+                            </ul>
+                    </div>
+                </div>
+                <div class="down">
+                    <div class="caracteristicas">
+                        <div class="peso"><p>Peso: 20 Kg</p></div>
+                        <div class="altura"><p>Altura: 9 M</p></div>
+                    </div>
+                </div> */
